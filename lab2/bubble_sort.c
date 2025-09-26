3e9ba807063a1b91d29f9f88817cb80d34bf398a
Borus25 <maksimborisov51046@gmail.com>
2025-09-26 12:50:29 +0700

    bubble_sort + bash

---
+++ lab2/bubble_sort.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/times.h>
+
+void bubble_sort(int arr[], int N) {
+    for (int j = N - 1; j > 0; j--) {
+        for (int i = 0; i < j; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int N;
+    printf("N: ");
+    if (scanf("%d", &N) != 1) {
+        printf("no input data!");
+        return 1;
+    }
+
+    if (N <= 0) {
+        printf("Incorrect array size!");
+        return 1;
+    }
+
+    int* arr = (int*)malloc(N * sizeof(int));
+    srand(12345);
+    for (int i = 0; i < N; i++) {
+        arr[i] = rand();
+    }
+
+    struct tms start_tms, end_tms;
+    long long ticks_per_sec = sysconf(_SC_CLK_TCK);
+
+    clock_t start_ticks = times(&start_tms);
+    bubble_sort(arr, N);
+    clock_t end_ticks = times(&end_tms);
+
+    double result = (double)(end_ticks - start_ticks) / ticks_per_sec;
+
+    printf("Time: %.6f sec\n", result);
+
+    for (int i = 1; i < N; i++) {
+        if (arr[i - 1] > arr[i]) {
+            printf("Массив не отсортирован\n");
+            break;
+        }
+    }
+    free(arr);
+    return 0;
+}
---
+++ lab2/build.sh
@@ -0,0 +1,7 @@
+gcc -Wall -Wextra -Werror bubble_sort.c -o bubble_sort
+gcc -Wall -Wextra -Werror -O1 bubble_sort.c -o bubble_sort
+gcc -Wall -Wextra -Werror -O2 bubble_sort.c -o bubble_sort
+gcc -Wall -Wextra -Werror -O3 bubble_sort.c -o bubble_sort
+gcc -Wall -Wextra -Werror -Os bubble_sort.c -o bubble_sort
+gcc -Wall -Wextra -Werror -Ofast bubble_sort.c -o bubble_sort
+gcc -Wall -Wextra -Werror -Og bubble_sort.c -o bubble_sort
